b58e359a039aa33971b7df87e649f054f2d0af00
Atta Mirza <atta-mirza@outlook.de>
2025-06-08 22:59:16 +0200

    refactoring

--- extract_commands.py
+++ extract_commands.py
@@ -5,27 +5,24 @@ def main():
     filepath = input(
         "Enter filepath, which contains terminal output (EXAMPLE: `/Users/atta/code/hf/scripts/session.txt`): "
     )
-    terminal_command_line_start = input(
-        "Enter terminal command line start (can be seperated by comma) (EXAMPLE:`(.venv) atta@ubuntuvm:~$,atta@ubuntuvm:~$)`: "
+    terminal_command_line_starts = input(
+        "Enter terminal command line starts (can be seperated by comma) (EXAMPLE:`(.venv) atta@ubuntuvm:~$,atta@ubuntuvm:~$)`: "
     )
 
-    command_starts = terminal_command_line_start.split(",")
+    command_starts = terminal_command_line_starts.split(",")
     with open(filepath, "r") as file:
         terminal_output = file.read()
 
-    executed_commands = []
-    for line in terminal_output.splitlines():
-        for prefix in command_starts:
-            prefix = prefix.strip()
-            if line.startswith(prefix):
-                command = line[len(prefix) :].strip()
-                executed_commands.append(command)
-                break
+    extracted_commands_file = "commands.txt"
+    with open(extracted_commands_file, "a") as file:
+        for line in terminal_output.splitlines():
+            for prefix in command_starts:
+                prefix = prefix.strip()
+                if line.startswith(prefix):
+                    command = line[len(prefix) :].strip()
+                    file.write(command + "\n")
 
-    with open("commands.txt", "w") as file:
-        for command in executed_commands:
-            file.write(command + "\n")
-    print("Commands extracted and saved to commands.txt")
+    print(f"Commands extracted and saved to {extracted_commands_file}")
     return
 
 
